1be7f02670619e4e8dfb7c2ea678abe0ccc39ccd
wajiddaudtamboli <wajiddaudtamboli123@gmail.com>
2025-06-22 18:04:05 +0530

    fix: use relative imports in Assissment.js to resolve Next.js module not found errors

--- app/careerplanning/checkcareer/components/Assissment.js
+++ app/careerplanning/checkcareer/components/Assissment.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription } from "@/components/ui/alert";
+import { Card, CardContent, CardHeader, CardTitle } from "../../../../../components/ui/card";
+import { Button } from "../../../../../components/ui/button";
+import { Alert, AlertDescription } from "../../../../../components/ui/alert";
 import { AiCareerFieldResult } from "../../../../../config/AllAiModels";
 
 const Assisment = ({
