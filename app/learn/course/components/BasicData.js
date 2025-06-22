67b764c97cc68e03a338392d195ec414cb6f7656
wajiddaudtamboli <wajiddaudtamboli123@gmail.com>
2025-06-22 21:21:38 +0530

    fix: correct all relative imports in BasicData.js for Next.js build compatibility

--- app/learn/course/components/BasicData.js
+++ app/learn/course/components/BasicData.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardContent } from "../../../../../components/ui/card";
+import { Input } from "../../../../../components/ui/input";
+import { Button } from "../../../../../components/ui/button";
 import {
   BookOpen,
   Clock,
@@ -13,7 +13,7 @@ import {
 
 import { useSearchParams } from "next/navigation";
 import { AiCourseContent } from "../../../../../config/AllAiModels";
-import LoadingDialog from "@/app/components/LoadingDialog";
+import LoadingDialog from "../../../../components/LoadingDialog";
 
 function BasicData({ activeStep, handleBack, handleNext, setContent }) {
   const searchParams = useSearchParams();
